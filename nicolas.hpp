748c9c4409274abd32c996b1c3eabc0afcd61137
Rojas <67125669+xrojas06@users.noreply.github.com>
2020-11-22 12:07:00 -0500

    Update nicolas.hpp

--- nicolas.hpp
+++ nicolas.hpp
@@ -5,28 +5,28 @@
 #include<stdlib.h>
 using namespace std;
 
-struct Move{
+struct Move{//ESTRUCTURA MOVA QUE GUARDA UNA CASILLA Y EL VALOR DE UTILIDAD ASOCIADO A DICHA CASILLA
   int i;
   int utilidad;
 };
+//CLASE TABLERO
 class tablero{
 private:
     vector<char> number;//VECTOR CON EL NUMERO DE CADA CASILLA EN CHAR
-    char win;
+    char win;//GANADOR DEL JUEGO
 public:
   tablero();//CONSTRUCTOR DE LA CLASE
   void print();//MOSTRAR EL TABLERO
-  void cleanall();
-  void cleanwin() { win = ' '; }
-  void clean();
-  void change(char s, int i);//INTERCAMBIAR EL NUMERO DE LA CASILLA POR EL SIMBOLO DEL JUGADOR
-  char getc(int i){return number[i];}
-  char getWin() { return win; }
-  bool empty(int p);//FUNCION PARA VERIFICAR SI UNA CASILLA ESPECIFICA(P) ESTA VACIA
-  bool pmove();
-  vector<int> freec();
-  bool winner(char s);
-  void move(char s, int i);
+  void cleanall();//REINICIAR TODAS LAS CASILLAS
+  void cleanwin() { win = ' '; }//REINICIAR EL GANADOR
+  void clean();//REINICIAR LAS CASILLAS Y EL GANADOR
+  char getWin() { return win; }//RETORNAR EL GANADOR
+  bool empty(int p);// VERIFICAR SI UNA CASILLA ESPECIFICA(P) ESTA VACIA
+  bool pmove();//VERIFICAR SI HAY CASILLAS VACIAS AUN
+  vector<int> freec();//GUARDAR QUE CASILLAS ESTAN VACIAS
+  bool winner(char s);//SABER SI GANO UN JUGADOR
+  void move(char s, int i); //REALIZAR UN MOVIMIENTO EN EL TABLERO
+  void cleanc(int i);//REINICIAR UNA CASILLAS
 };
 
 //CLASE PERSON PARA EL JUGADOR QUE VA EN CONTRA DEL ALGORITMO
@@ -34,19 +34,32 @@ class Person{
 private:
   char s;//SIMBOLO DEL JUGADOR: 'O'
 public:
-  Person(char sb){ s = sb;}//CONSTRUCTOR CLASE PERSON
+  Person(){ s = 'O';}//CONSTRUCTOR CLASE PERSON
   int movep(tablero leo);//FUNCION PARA RETORNAR EL NUMERO DE CASILLA ELEGIDA POR EL USUARIO
   char get_psymbol(){ return s;}//RETORNAR EL SIMBOLO DEL JUGADOR
 };
 
 class SmartPlayer{
 private:
-  char s;
-  Move SPrandommove(vector<int> c);
+  char s;//SIMBOLO DEL JUGADOR: 'x'
 public:
-  SmartPlayer(char sb){ s = sb;}
-  int bestmove(tablero leo);
-  char get_Spsymbol(){ return s;}
-  Move minimax(tablero red, char blue);
+  SmartPlayer(){ s = 'X';}// CONSTRUCTOR
+  int bestmove(tablero leo);//RETORNA LA CASILLA SEGUN EL MEJOR MOVIMIENTO
+  char get_Spsymbol(){ return s;}//RETORNA EL SIMBOLO DEL JUGADOR
+  Move minimax(tablero inicial, char ismax);//ELIGE EL MEJOR MOVIMIENTO SEGUN EL TABLERO Y UN CARACTER
+};
+
+class Triqui{
+private:
+  tablero b;//EL TABLERO DE JUEGO
+  Person player;//EL USUARIO QUE VA A JUGAR
+  SmartPlayer bestcompu;// EL JUGADOR QUE TRATARA DE GANAR O EMPATAR
+  int turns;//UN CONTADOR QUE GUARDA EL TURNO EN EL QUE VA EL JUEGO
+  void vc(int n, char jugada);//FUNCION PRIVADA QUE VERIFICA SI UNA CASILLA ESTA VACIA Y SI SI HACE EL CAMBIO DE ESTA POR EL SIMBOLO DEL JUGADOR
+public:
+  Triqui();//CONSTRUCTOR DE LA CLASE
+  void smartgame();//MODALIDAD DEL JUEGO CONTRA EL OPONENTE QUE BUSCA NO PERDER
+  bool conti();//VERIFICAR SI SE PUEDE CONTINUAR EL JUEGO
+  void printwinner();//MOSTRAR LE GANADOR O SI HUBO EMPATE
 };
 #endif
